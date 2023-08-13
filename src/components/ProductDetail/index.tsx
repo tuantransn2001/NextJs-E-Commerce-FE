44a9417695cf49618f38a40b1b5af133803fe147
Tuấn Trần <tuantransn2001@gmail.com>
2023-08-13 18:32:48 +0700

    fix page productDetail not found

--- src/components/ProductDetail/index.tsx
+++ src/components/ProductDetail/index.tsx
@@ -7,17 +7,18 @@ import { useGet, useGetURLParams } from '@/customizes/hooks';
 import Page from '../helpers/Page';
 import { API_PATH } from '@/ts/enums/api_enums';
 import LoadingScreen from '../helpers/LoadingScreen';
-import { isEmpty } from '@/common';
+import { handleGetHrefArr, isEmpty } from '@/common';
 const cx = classNames.bind(require('./style/ProductDetailPage.module.scss'));
 
 const ProductDetail = ({}) => {
-  const [id] = useGetURLParams();
+  const params = useGetURLParams();
+  const hrefID = handleGetHrefArr()[handleGetHrefArr().length - 1];
 
   const { data: productDetail, isLoading } = useGet(API_PATH.getOneProduct, {
-    id,
+    id: params ? params[0] : hrefID,
   });
 
-  const isNotOK = isEmpty(productDetail) || isLoading;
+  const isNotOK = isEmpty(productDetail) || isLoading || !params;
 
   return isNotOK ? (
     <LoadingScreen />
